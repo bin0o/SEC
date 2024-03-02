c6c7ee04664011260fdf181ec97b5ec808fdbf88
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2024-03-02 18:25:32 +0000

    Implemented Authenticated Perfect Links (APLs)

---
+++ Communication/src/main/java/pt/ulisboa/tecnico/hdsledger/communication/AppendReplyMessage.java
@@ -0,0 +1,35 @@
+package pt.ulisboa.tecnico.hdsledger.communication;
+
+import com.google.gson.Gson;
+
+public class AppendReplyMessage {
+
+    private final boolean confirmation;
+
+    private final int index;
+
+    public AppendReplyMessage(boolean confirmation, int index) {
+        this.confirmation = confirmation;
+        this.index = index;
+    }
+
+    public boolean getConfirmation() {
+        return this.confirmation;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
+    public String toJson() {
+        return new Gson().toJson(this);
+    }
+
+    @Override
+    public String toString() {
+        return "AppendReplyMessage{" +
+                "confirmation=" + confirmation +
+                ", index=" + index +
+                '}';
+    }
+}
